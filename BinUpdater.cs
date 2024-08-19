39562287d28b3c67d11a56ed2bb674237d3161b4
Äkwav <ekwav@coflnet.com>
2024-08-19 18:26:53 +0200

    cachebust retry

--- BinUpdater.cs
+++ BinUpdater.cs
@@ -14,6 +14,7 @@ namespace Coflnet.Sky.Updater
     public class BinUpdater
     {
         private List<string> apiKeys = new List<string>();
+        private static string LastFirst = null;
 
         public BinUpdater(IEnumerable<string> apiKeys)
         {
@@ -34,8 +35,30 @@ namespace Coflnet.Sky.Updater
 
         public static async Task<List<SaveAuction>> DownloadSells(string BaseUrl)
         {
+            string firstFoundUuid = null;
+            List<SaveAuction> auctions = new List<SaveAuction>();
+            while (firstFoundUuid == LastFirst)
+            {
+                auctions = await DownloadAndParse(BaseUrl).ConfigureAwait(false);
+                firstFoundUuid = auctions.FirstOrDefault()?.Uuid;
+                if (firstFoundUuid == LastFirst)
+                {
+                    Console.WriteLine("No new auctions found, waiting 1s");
+                    await Task.Delay(1000);
+                }
+            }
+            if (Updater.updaterIndex % 2 == 0)
+                Console.WriteLine($"Updated {auctions.Count} bin sells eg {firstFoundUuid}");
+            else
+                Console.WriteLine($"Updated {auctions.Count} bin sells {string.Join(',', auctions.Select(a => a.Uuid))}");
+            LastFirst = firstFoundUuid;
+            return auctions;
+        }
+
+        private static async Task<List<SaveAuction>> DownloadAndParse(string BaseUrl)
+        {
             var client = new RestClient(BaseUrl);
-            var request = new RestRequest($"/v2/skyblock/auctions_ended", Method.Get);
+            var request = new RestRequest($"/v2/skyblock/auctions_ended?cache=" + System.Net.Dns.GetHostName(), Method.Get);
 
             //Get the response and Deserialize
             var response = await client.ExecuteAsync(request).ConfigureAwait(false);
@@ -67,10 +90,6 @@ namespace Coflnet.Sky.Updater
 
                 return a;
             }).ToList();
-            if (Updater.updaterIndex % 2 == 0)
-                Console.WriteLine($"Updated {expired.Auctions.Count} bin sells eg {expired.Auctions.FirstOrDefault()?.Uuid}");
-            else
-                Console.WriteLine($"Updated {expired.Auctions.Count} bin sells {string.Join(',', auctions.Select(a => a.Uuid))}");
             return auctions;
         }
     }
