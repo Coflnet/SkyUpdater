09306d27a27e0dc843d21dbfc7a596492e81c019
Äkwav <ekwav@coflnet.com>
2025-02-03 23:10:44 +0100

    use filescope namespace for auction page

--- Models/AuctionsPage.cs
+++ Models/AuctionsPage.cs
@@ -2,39 +2,37 @@ using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
-namespace Coflnet.Sky.Updater.Models
+namespace Coflnet.Sky.Updater.Models;
+public class AuctionPage
 {
-    public class AuctionPage
+    [JsonPropertyName("success")]
+    public bool WasSuccessful { get; set; }
+
+    [JsonPropertyName("cause")]
+    public string Cause { get; set; }
+
+    [JsonPropertyName("page")]
+    public long Page { get; set; }
+
+    [JsonPropertyName("totalPages")]
+    public long TotalPages { get; set; }
+
+    [JsonPropertyName("totalAuctions")]
+    public long TotalAuctions { get; set; }
+
+    [JsonPropertyName("lastUpdated")]
+    public long _lastUpdated { get; set; }
+    public DateTime LastUpdated
     {
-        [JsonPropertyName("success")]
-        public bool WasSuccessful { get; set; }
-
-        [JsonPropertyName("cause")]
-        public string Cause { get; set; }
-
-        [JsonPropertyName("page")]
-        public long Page { get; set; }
-
-        [JsonPropertyName("totalPages")]
-        public long TotalPages { get; set; }
-
-        [JsonPropertyName("totalAuctions")]
-        public long TotalAuctions { get; set; }
-
-        [JsonPropertyName("lastUpdated")]
-        public long _lastUpdated { get; set; }
-        public DateTime LastUpdated
+        get
         {
-            get
-            {
-                var convertToDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-                convertToDateTime = convertToDateTime.AddMilliseconds(_lastUpdated).ToLocalTime();
-                return convertToDateTime;
-            }
-
+            var convertToDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            convertToDateTime = convertToDateTime.AddMilliseconds(_lastUpdated).ToLocalTime();
+            return convertToDateTime;
         }
 
-        [JsonPropertyName("auctions")]
-        public List<Auction> Auctions { get; set; }
     }
+
+    [JsonPropertyName("auctions")]
+    public List<Auction> Auctions { get; set; }
 }
