71c7367fc95031d007f37acca2f0a803bd683f04
Äkwav <ekwav@coflnet.com>
2026-04-28 18:08:23 +0200

    update Program namespace usage

--- Program.cs
+++ Program.cs
@@ -2,21 +2,20 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 
-namespace Coflnet.Sky.Updater
-{
-    public class Program
-    {
-        //static string apiKey = SimplerConfig.Config.Instance["apiKey"];
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+namespace Coflnet.Sky.Updater;
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+public class Program
+{
+    //static string apiKey = SimplerConfig.Config.Instance["apiKey"];
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
     }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
