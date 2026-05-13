3aa83e9d33d6ad84c291503e9cf67b221554501e
Ekwav <ekwav@users.noreply.github.com>
2026-05-13 14:31:34 +0200

    switch startup.cs to file scoped namespace

--- Startup.cs
+++ Startup.cs
@@ -8,61 +8,60 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi;
 using Prometheus;
 
-namespace Coflnet.Sky.Updater
+namespace Coflnet.Sky.Updater;
+
+public class Startup
 {
-    public class Startup
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+        services.AddControllers();
+        services.AddSwaggerGen(c =>
         {
-            Configuration = configuration;
-        }
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "SkyUpdater", Version = "v1" });
+        });
+        services.AddJaeger(Configuration, 0.1);
+        services.AddSingleton<ItemSkinHandler>();
+        services.AddHostedService<ItemSkinHandler>(di => di.GetService<ItemSkinHandler>());
+        services.AddHostedService<UpdaterManager>();
+        services.AddSingleton<Kafka.KafkaCreator>();
+        services.AddSingleton<ItemDetailsExtractor>();
+        services.AddSingleton<ItemDetails>();
+        services.AddSingleton<Topics>(di => Configuration.GetSection("TOPICS").Get<Topics>());
+        services.AddSingleton<Items.Client.Api.IItemsApi, Items.Client.Api.ItemsApi>(sp => new Items.Client.Api.ItemsApi(Configuration["ITEMS_BASE_URL"]));
+        services.AddResponseCaching();
+        services.AddMemoryCache();
+    }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
         {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SkyUpdater", Version = "v1" });
-            });
-            services.AddJaeger(Configuration, 0.1);
-            services.AddSingleton<ItemSkinHandler>();
-            services.AddHostedService<ItemSkinHandler>(di => di.GetService<ItemSkinHandler>());
-            services.AddHostedService<UpdaterManager>();
-            services.AddSingleton<Kafka.KafkaCreator>();
-            services.AddSingleton<ItemDetailsExtractor>();
-            services.AddSingleton<ItemDetails>();
-            services.AddSingleton<Topics>(di => Configuration.GetSection("TOPICS").Get<Topics>());
-            services.AddSingleton<Items.Client.Api.IItemsApi, Items.Client.Api.ItemsApi>(sp => new Items.Client.Api.ItemsApi(Configuration["ITEMS_BASE_URL"]));
-            services.AddResponseCaching();
-            services.AddMemoryCache();
+            app.UseDeveloperExceptionPage();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "SkyUpdater v1");
-                c.RoutePrefix = "api";
-            });
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "SkyUpdater v1");
+            c.RoutePrefix = "api";
+        });
 
-            app.UseRouting();
-            app.UseResponseCaching();
+        app.UseRouting();
+        app.UseResponseCaching();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapMetrics();
-                endpoints.MapControllers();
-            });
-        }
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapMetrics();
+            endpoints.MapControllers();
+        });
     }
 }
