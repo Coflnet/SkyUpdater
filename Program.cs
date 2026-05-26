2b47e7ec1661a51f882fedd5e08ca1967839002b
Ekwav <ekwav@users.noreply.github.com>
2026-05-26 21:06:32 +0200

    Add openbao configuration provider

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Coflnet.Security.OpenBao;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -14,6 +15,7 @@ public class Program
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((_, config) => config.AddOpenBaoFromEnvironment())
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
