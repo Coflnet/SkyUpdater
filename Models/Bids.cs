5833fbc331360e2f9d990786aeed99ca9942d86b
Äkwav <ekwav@coflnet.com>
2026-07-23 23:13:33 +0200

    move bids to file scoped namespace

--- Models/Bids.cs
+++ Models/Bids.cs
@@ -1,37 +1,36 @@
 using System;
 using System.Text.Json.Serialization;
 
-namespace Coflnet.Sky.Updater.Models
+namespace Coflnet.Sky.Updater.Models;
+
+public class Bids
 {
-    public class Bids
+    [JsonPropertyName("auction_id")]
+    [Newtonsoft.Json.JsonProperty("auction_id")]
+    public string AuctionId { get; set; }
+
+    [JsonPropertyName("bidder")]
+    public string Bidder { get; set; }
+
+    [JsonPropertyName("profile_id")]
+    [Newtonsoft.Json.JsonProperty("profile_id")]
+    public string ProfileId { get; set; }
+
+    [JsonPropertyName("amount")]
+    public long Amount { get; set; }
+
+    [JsonPropertyName("timestamp")]
+    [Newtonsoft.Json.JsonProperty("timestamp")]
+    public long _timestamp { get; set; }
+    [Newtonsoft.Json.JsonIgnore]
+    public DateTime Timestamp
     {
-        [JsonPropertyName("auction_id")]
-        [Newtonsoft.Json.JsonProperty("auction_id")]
-        public string AuctionId { get; set; }
-
-        [JsonPropertyName("bidder")]
-        public string Bidder { get; set; }
-
-        [JsonPropertyName("profile_id")]
-        [Newtonsoft.Json.JsonProperty("profile_id")]
-        public string ProfileId { get; set; }
-
-        [JsonPropertyName("amount")]
-        public long Amount { get; set; }
-
-        [JsonPropertyName("timestamp")]
-        [Newtonsoft.Json.JsonProperty("timestamp")]
-        public long _timestamp { get; set; }
-        [Newtonsoft.Json.JsonIgnore]
-        public DateTime Timestamp
+        get
         {
-            get
-            {
-                var convertToDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-                convertToDateTime = convertToDateTime.AddMilliseconds(_timestamp).ToLocalTime();
-                return convertToDateTime;
-            }
-
+            var convertToDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            convertToDateTime = convertToDateTime.AddMilliseconds(_timestamp).ToLocalTime();
+            return convertToDateTime;
         }
+
     }
 }
